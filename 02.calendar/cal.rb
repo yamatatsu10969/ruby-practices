0f36e738ba9753fac69853619c1e403e3c2f5e6f
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-01-16 11:51:41 +0900

    曜日を表示するメソッド追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,10 +2,18 @@
 
 require 'date'
 
-# 今日の日付を表示
+# 今日の [月・年] を表示
 def print_today
   @today = Date.today
   puts("#{@today.month}月 #{@today.year}年")
 end
 
+
+
+# 曜日を表示
+def print_day_of_week
+  puts("日 月 火 水 木 金 土")
+end
+
 print_today
+print_day_of_week
