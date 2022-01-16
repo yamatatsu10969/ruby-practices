a669153656e723209e22ea10fc208b666fbfbc5d
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-01-16 12:23:18 +0900

    日付部分を表示するロジックの追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,15 +5,44 @@ require 'date'
 # 今日の [月・年] を表示
 def print_today
   @today = Date.today
-  puts("#{@today.month}月 #{@today.year}年")
+  puts("      #{@today.month}月 #{@today.year}年")
 end
-
-
 
 # 曜日を表示
 def print_day_of_week
-  puts("日 月 火 水 木 金 土")
+  puts(" 日 月 火 水 木 金 土")
+end
+
+# Dateから日付を表示する
+def print_dates(inputDate)
+  @first_date = Date.new(inputDate.year, inputDate.month, 1)
+  @end_date = Date.new(inputDate.year, inputDate.month, -1)
+
+  for date in @first_date..@end_date
+    @print_day = date.mday.to_s
+
+    # 数字が一桁の場合、インデントがずれるため空文字を先頭に追加
+    if (@print_day.length == 1)
+      @print_day = " " + @print_day
+    end
+
+    # 最初の日付(1日)の場合はインデントを調整する
+    if (date.mday == 1 && date.wday != 0)
+      @print_day = "   " * date.wday + @print_day
+    end
+
+    # 土曜日の後は改行する
+    if (date.saturday?)
+      @print_day = @print_day + "\n"
+    end
+
+    # print の出力は改行しない
+    # " " の空文字を入れることで前の数字とのスペースを作成
+    print(" ", @print_day)
+  end
 end
 
 print_today
 print_day_of_week
+
+print_dates(Date.new(2021, 1))
