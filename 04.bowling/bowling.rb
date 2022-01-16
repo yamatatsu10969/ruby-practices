ad405e00b66e1307cb0290cf74598bb93831bc67
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-01-17 01:23:38 +0900

    正しい出力ができるように修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,13 +1,15 @@
-# sample_input = "6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4" # これ間違っている
+# sample_input = "6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5" # 139
 
-# sample_input = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-sample_input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-# sample_input = 'X,X,X,X,X,X,X,X,X,X,X,X'
+# sample_input = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4' # 107
+# sample_input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0' # 134
+# sample_input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X' # 164
+sample_input = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8' # 144
+# sample_input = 'X,X,X,X,X,X,X,X,X,X,X,X' # 300
 
 frame_count = 10
 strike_sign = 'X'
 
-# 引数をもらう
+# TODO: 引数をもらう
 
 # 文字列から配列に分解する
 scores = sample_input.split(',')
@@ -34,29 +36,27 @@ end
 
 p current_frame
 p frame_scores
-# p frame_int_scores
 
 frame_scores.map!.with_index do |scores, frame_index|
   p frame_index
-  if (frame_index == 9)
-    p 'hogehog'
-    scores
+  if frame_index == 9
+    next scores
   end
 
   next_frame_score = frame_scores[frame_index + 1]
-  # if scores.include?(strike_sign)
-  #   if next_frame_score.include?(strike_sign && next_frame_score.length == 1)
-  #     after_next_frame_score = after_next_frame_score[frame_index + 2]
-  #     [strike_sign, strike_sign, after_next_frame_score[0]]
-  #   else
-  #     [strike_sign, next_frame_score[0], next_frame_score[1]]
-  #   end
-  # end
+  if scores.include?(strike_sign)
+    if next_frame_score.include?(strike_sign) && next_frame_score.length == 1
+      after_next_frame_score = frame_scores[frame_index + 2]
+      next [strike_sign, strike_sign, after_next_frame_score[0]]
+    else
+      next [strike_sign, next_frame_score[0], next_frame_score[1]]
+    end
+  end
 
   if [scores[0].to_i, scores[1].to_i].sum == 10
-    scores.push(next_frame_score[0])
+    next scores.push(next_frame_score[0])
   else
-    scores
+    next scores
   end
 end
 
@@ -65,63 +65,14 @@ p frame_scores
 
 # すべて数字に変更
 frame_scores.map! do |scores|
-  if scores.include?(strike_sign)
-    [10]
-  else
-    [scores.first.to_i, scores.last.to_i]
+  scores.map! do | score |
+    if score == strike_sign
+      next 10
+    else
+      next score.to_i
+    end
   end
+  next scores.sum
 end
 
-p frame_scores
-
-
-# is_previous_score_spare = false
-# is_previous_score_strike = false
-# total_score = 0
-
-# frame_scores.map.with_index do |scores, frame_index|
-#   add_score = 0
-#   p scores
-#   scores.map.with_index do |score, score_index|
-#     score_number = score == strike_sign ? 10 : score
-#     add_score = score_number
-
-#     if is_previous_score_spare
-#       p 'スペア分を足すよ'
-#       add_score += score_number
-#       is_previous_score_spare = false
-#     elsif is_previous_score_strike
-#       p 'ストライク分を足すよ'
-#       add_score += score_number
-#     end
-#     if frame_index > 1
-#       if frame_scores[frame_index - 1] == [10] && frame_scores[frame_index - 2] == [10] && score_index != 1
-#         p 'ストライクの余剰分をさらに足すよ'
-#         add_score += score_number
-#       end
-#     end
-
-
-#     total_score += add_score
-#     print 'add: '
-#     p add_score
-
-#   end
-
-#   print 'total: '
-#   p total_score
-
-#   if scores == [10]
-#     p 'ストライクだよ！'
-#     is_previous_score_strike = true
-#   else
-#     int_scores = scores.map { |score| score }
-#     if int_scores.sum == 10
-#       p 'スペア~~~'
-#       is_previous_score_spare = true
-#     else
-#       is_previous_score_strike = false
-#     end
-#   end
-#   p '----------------'
-# end
+p frame_scores.sum
