84ce4dbd885d35d6eb8a80e854a14bdf9279328f
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-01-18 13:56:50 +0900

    rubocop のワーニングを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # sample_input = "6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5" # 139
 
 # sample_input = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4' # 107
@@ -12,7 +14,7 @@ strike_sign = 'X'
 # TODO: 引数をもらう
 
 # 文字列から配列に分解する
-scores = sample_input.split(',')
+string_scores = sample_input.split(',')
 
 # 10個の配列に分けて、フレーム単位にする
 frame_scores = []
@@ -24,7 +26,7 @@ p frame_scores.length
 
 # 10個の配列にスコアを格納
 current_frame = 1
-scores.each do |score|
+string_scores.each do |score|
   current_scores_index = current_frame - 1
   current_scores = frame_scores[current_scores_index]
   current_scores.push(score)
@@ -39,23 +41,22 @@ p frame_scores
 
 frame_scores.map!.with_index do |scores, frame_index|
   p frame_index
+
   next scores if frame_index == 9
 
   next_frame_score = frame_scores[frame_index + 1]
   if scores.include?(strike_sign)
     if next_frame_score.include?(strike_sign) && next_frame_score.length == 1
       after_next_frame_score = frame_scores[frame_index + 2]
-      next [strike_sign, strike_sign, after_next_frame_score[0]]
+      [strike_sign, strike_sign, after_next_frame_score[0]]
     else
-      next [strike_sign, next_frame_score[0], next_frame_score[1]]
+      [strike_sign, next_frame_score[0], next_frame_score[1]]
     end
   end
 
-  if [scores[0].to_i, scores[1].to_i].sum == 10
-    next scores.push(next_frame_score[0])
-  else
-    next scores
-  end
+  next scores.push(next_frame_score[0]) if [scores[0].to_i, scores[1].to_i].sum == 10
+
+  next scores
 end
 
 p frame_scores
@@ -64,9 +65,9 @@ p frame_scores
 frame_scores.map! do |scores|
   scores.map! do |score|
     if score == strike_sign
-      next 10
+      10
     else
-      next score.to_i
+      score.to_i
     end
   end
   next scores.sum
