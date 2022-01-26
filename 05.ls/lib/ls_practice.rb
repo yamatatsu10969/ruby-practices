858c0aff42b9915d8e826587c82e51ffe05108cf
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-01-26 17:25:18 +0900

    テストケースの追加とLSクラスの仮実装

--- 05.ls/lib/ls_practice.rb
+++ 05.ls/lib/ls_practice.rb
@@ -1,1 +1,17 @@
-puts 'hoge'
+# frozen_string_literal: true
+
+def main
+  LS.show
+end
+
+class LS
+  def self.show
+    puts Dir.pwd
+    puts Dir.getwd
+    puts Dir.glob('*')
+    puts Dir.pwd.encode(Encoding::UTF_8)
+    p Dir.glob('*')
+  end
+end
+
+main
--- 05.ls/test/test_ls.rb
+++ 05.ls/test/test_ls.rb
@@ -1,8 +1,21 @@
-require 'minitest/autorun'
+# frozen_string_literal: true
 
+require 'minitest/autorun'
+require_relative '../lib/ls_practice'
 class LsTest < Minitest::Test
   def test_ls_without_option
-    expected = 'hoge'
-    assert_equal(expected, 'hoge')
+    expected = <<~TEXT
+      hoge1.txt               hoge6.txt               hogehogehoge2.txt
+      hoge10.txt              hoge7.txt               hogehogehoge3.txt
+      hoge11.txt              hoge8.txt               hogehogehoge4.txt
+      hoge12.txt              hoge9.txt               hogehogehoge5.txt
+      hoge13.txt              hogehogehoge1.txt       hogehogehoge6.txt
+      hoge2.txt               hogehogehoge10.txt      hogehogehoge7.txt
+      hoge3.txt               hogehogehoge11.txt      hogehogehoge8.txt
+      hoge4.txt               hogehogehoge12.txt      hogehogehoge9.txt
+      hoge5.txt               hogehogehoge13.txt
+    TEXT
+    puts expected
+    assert_equal(expected, main)
   end
 end
