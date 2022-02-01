2fb1595f19bc745e595ceb62bd7ee5ada83f03db
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-02-01 17:46:21 +0900

    p メソッドは開発用なので puts に変更
    シングルクォートだと改行コードを認識してくれないので %() とダブルクォートを使用

--- 05.ls/lib/ls_practice.rb
+++ 05.ls/lib/ls_practice.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 def main
-  LS.new.show
+  puts LS.new.show
 end
 
 class LS
@@ -10,52 +10,24 @@ class LS
   def show
     files = Dir.glob('*')
     row_item_length = files.length / MAX_ROW_LENGTH
+
     max_text_length = files.map(&:length).max + 6
+
+    text = %()
 
     (0..row_item_length).each do |n|
       (0..(MAX_ROW_LENGTH - 1)).map do |i|
         index = n + i + (i * row_item_length)
-        printf("%-#{max_text_length}s", files[index])
-        puts '' if (MAX_ROW_LENGTH - 1) == i
+        if (MAX_ROW_LENGTH - 1) == i
+          text += files[index].to_s
+          text += "\n"
+        else
+          text += format("%-#{max_text_length}s", files[index])
+        end
       end
     end
+    text
   end
 end
 
 main
-
-# 以下でテストを通したいが、どうしても \n が \\n になってしまう
-
-# frozen_string_literal: true
-
-# def main
-#   LS.new.show
-# end
-
-# class LS
-#   MAX_ROW_LENGTH = 3
-
-#   def show
-#     files = Dir.glob('*')
-#     row_item_length = files.length / MAX_ROW_LENGTH
-
-#     max_text_length = files.map(&:length).max + 6
-
-#     text = ''
-
-#     (0..row_item_length).each do |n|
-#       (0..(MAX_ROW_LENGTH - 1)).map do |i|
-#         index = n + i + (i * row_item_length)
-#         if (MAX_ROW_LENGTH - 1) == i
-#           text += files[index].to_s
-#           text += '\n'
-#         else
-#           text += format("%-#{max_text_length}s", files[index])
-#         end
-#       end
-#     end
-#     p text
-#   end
-# end
-
-# main
