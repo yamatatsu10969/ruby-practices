4a870a7c28b8377f957662549beab5710ed96372
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-02-04 23:08:23 +0900

    length が長さを表してるように思えるので number に変更

--- 05.ls/lib/ls_practice.rb
+++ 05.ls/lib/ls_practice.rb
@@ -5,24 +5,24 @@ def main
 end
 
 class LS
-  MAX_COLUMN_LENGTH = 3
-  MAX_COLUMN_LENGTH.freeze
+  MAX_COLUMN_NUMBER = 3
+  MAX_COLUMN_NUMBER.freeze
 
   WHITE_SPACE_INDENT_LENGTH = 6
   WHITE_SPACE_INDENT_LENGTH.freeze
 
   def create_files_and_folders_text
     files = Dir.glob('*')
-    column_item_length = files.length / MAX_COLUMN_LENGTH
+    column_item_number = files.length / MAX_COLUMN_NUMBER
 
     max_text_length = files.map(&:length).max + WHITE_SPACE_INDENT_LENGTH
 
     text = %()
 
-    (0..column_item_length).each do |column_index|
-      (0..(MAX_COLUMN_LENGTH - 1)).map do |row_index|
-        file_index = column_index + row_index + (row_index * column_item_length)
-        if (MAX_COLUMN_LENGTH - 1) == row_index
+    (0..column_item_number).each do |column_index|
+      (0..(MAX_COLUMN_NUMBER - 1)).map do |row_index|
+        file_index = column_index + row_index + (row_index * column_item_number)
+        if (MAX_COLUMN_NUMBER - 1) == row_index
           text += files[file_index].to_s
           text += "\n"
         else
