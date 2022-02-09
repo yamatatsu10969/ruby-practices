157bf26f6ec4c05d6f68d9ee5b6e7e2bec93a3fc
yamatatsu10969 <52.wadachi.96@gmail.com>
2022-02-10 08:19:07 +0900

    テストケース作成

--- 05.ls/test/test_ls.rb
+++ 05.ls/test/test_ls.rb
@@ -17,4 +17,20 @@ class LsTest < Minitest::Test
     TEXT
     assert_equal(expected.rstrip, LS.new.create_files_and_folders_text)
   end
+
+  def test_ls_with_a_option
+    expected = <<~TEXT
+      .                       hoge6.txt               hogehogehoge3.txt
+      hoge1.txt               hoge7.txt               hogehogehoge4.txt
+      hoge10.txt              hoge8.txt               hogehogehoge5.txt
+      hoge11.txt              hoge9.txt               hogehogehoge6.txt
+      hoge12.txt              hogehogehoge1.txt       hogehogehoge7.txt
+      hoge13.txt              hogehogehoge10.txt      hogehogehoge8.txt
+      hoge2.txt               hogehogehoge11.txt      hogehogehoge9.txt
+      hoge3.txt               hogehogehoge12.txt
+      hoge4.txt               hogehogehoge13.txt
+      hoge5.txt               hogehogehoge2.txt
+    TEXT
+    assert_equal(expected.rstrip, LS.new.create_files_and_folders_text)
+  end
 end
