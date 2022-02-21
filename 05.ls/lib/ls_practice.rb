4ba5d412c319ed5045c6397786da5058a5e98502
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-02-22 00:08:58 +0900

    Module を使用してクラス当たりのメソッド量を少なくした

--- 05.ls/lib/ls_practice.rb
+++ 05.ls/lib/ls_practice.rb
@@ -33,8 +33,128 @@ module Option
   end
 end
 
+module PermissionFormat
+  def permissions(stat_files)
+    stat_files.map do |file|
+      mode = file.mode.to_s(8)
+      permission = mode[- 3..]
+      file_owner_permission = permission[0].to_i.to_s(2)
+      file_group_permission = permission[1].to_i.to_s(2)
+      other_permission = permission[2].to_i.to_s(2)
+      to_permission_string(file_owner_permission) + to_permission_string(file_group_permission) + to_permission_string(other_permission)
+    end
+  end
+
+  def to_permission_string(permission)
+    permission.split('').map.each_with_index do |string, index|
+      if string == '0'
+        '-'
+      elsif index.zero?
+        'r'
+      elsif index == 1
+        'w'
+      else
+        'x'
+      end
+    end.join('')
+  end
+end
+
+module LongFormat
+  include PermissionFormat
+  def long_format_files_and_folders_text(files)
+    stat_files = to_stat_files(files)
+    text_array = to_file_information_array(stat_files, files)
+    text_array.map(&:rstrip).join("\n")
+  end
+
+  def to_file_information_array(stat_files, files)
+    permissions = permissions(stat_files)
+    file_types = file_types(stat_files)
+    hard_link_numbers = hard_link_numbers(stat_files)
+    user_names = user_names(stat_files)
+    group_names = group_names(stat_files)
+    file_sizes = file_sizes(stat_files)
+    last_update_date_times = last_update_date_times(stat_files)
+    names_and_symlink_names = names_and_symlink_names(files)
+    text_array = ["total #{total_blocks(stat_files)}"]
+    permissions.size.times do |index|
+      text = "#{file_types[index]}#{permissions[index]}  "
+      text += "#{hard_link_numbers[index].to_s.rjust(hard_link_numbers.map(&:to_s).map(&:length).max)} "
+      text += "#{user_names[index].ljust(user_names.map(&:length).max)}  "
+      text += "#{group_names[index].ljust(group_names.map(&:length).max)}  "
+      text += "#{file_sizes[index].to_s.rjust(file_sizes.map(&:to_s).map(&:length).max)} "
+      text += "#{last_update_date_times[index].ljust(last_update_date_times.map(&:length).max)} "
+      text += names_and_symlink_names[index]
+      text_array.push(text)
+    end
+    text_array
+  end
+
+  def to_stat_files(stat_files)
+    stat_files.map do |file|
+      if FileTest.symlink?(file)
+        File.lstat(file)
+      else
+        File.stat(file)
+      end
+    end
+  end
+
+  def names_and_symlink_names(files)
+    files.map do |file|
+      if FileTest.symlink?(file)
+        "#{file} -> #{File.readlink(file)}"
+      else
+        file.to_s
+      end
+    end
+  end
+
+  def total_blocks(stat_files)
+    stat_files.sum(&:blocks)
+  end
+
+  def hard_link_numbers(stat_files)
+    stat_files.map(&:nlink)
+  end
+
+  def user_names(stat_files)
+    stat_files.map do |file|
+      Etc.getpwuid(file.uid).name
+    end
+  end
+
+  def file_sizes(stat_files)
+    stat_files.map(&:size)
+  end
+
+  def group_names(stat_files)
+    stat_files.map do |file|
+      Etc.getgrgid(file.gid).name
+    end
+  end
+
+  def last_update_date_times(stat_files)
+    stat_files.map do |file|
+      file.mtime.strftime('%b %d %H:%M')
+    end
+  end
+
+  def file_types(files)
+    files.map do |file|
+      if file.ftype == 'file'
+        '-'
+      else
+        file.ftype[0]
+      end
+    end
+  end
+end
+
 class LS
   include Option
+  include LongFormat
 
   MAX_COLUMN_NUMBER = 3
   MAX_COLUMN_NUMBER.freeze
@@ -73,117 +193,6 @@ class LS
       Dir.glob('*', File::FNM_DOTMATCH)
     else
       Dir.glob('*')
-    end
-  end
-
-  def long_format_files_and_folders_text(files)
-    stat_files = to_stat_files(files)
-    text_array = []
-    text_array.push("total #{total_blocks(stat_files)}")
-    permissions = permissions(stat_files)
-    file_types =  file_types(stat_files)
-    hard_link_numbers = hard_link_numbers(stat_files)
-    user_names = user_names(stat_files)
-    group_names = group_names(stat_files)
-    file_sizes = file_sizes(stat_files)
-    last_update_date_times = last_update_date_times(stat_files)
-    names_and_symlink_names = names_and_symlink_names(files)
-    (0..(files.size - 1)).each do |index|
-      text = ''
-      text += file_types[index] + permissions[index] + '  '
-      text += hard_link_numbers[index].to_s.rjust(hard_link_numbers.map(&:to_s).map(&:length).max) + ' '
-      text += user_names[index].ljust(user_names.map(&:length).max) + '  '
-      text += group_names[index].ljust(group_names.map(&:length).max) + '  '
-      text += file_sizes[index].to_s.rjust(file_sizes.map(&:to_s).map(&:length).max) + ' '
-      text += last_update_date_times[index].ljust(last_update_date_times.map(&:length).max) + ' '
-      text += names_and_symlink_names[index]
-      text_array.push(text)
-    end
-    text_array.map(&:rstrip).join("\n")
-  end
-
-  def to_stat_files(stat_files)
-    stat_files.map do |file|
-      if FileTest.symlink?(file)
-        File.lstat(file)
-      else
-        File.stat(file)
-      end
-    end
-  end
-
-  def names_and_symlink_names(files)
-    files.map do |file|
-      if FileTest.symlink?(file)
-        file.to_s + ' -> ' + File.readlink(file.to_s)
-      else
-        file.to_s
-      end
-    end
-  end
-
-  def total_blocks(stat_files)
-    stat_files.sum(&:blocks)
-  end
-
-  def hard_link_numbers(stat_files)
-    stat_files.map(&:nlink)
-  end
-
-  def user_names(stat_files)
-    stat_files.map do |file|
-      Etc.getpwuid(file.uid).name
-    end
-  end
-
-  def file_sizes(stat_files)
-    stat_files.map(&:size)
-  end
-
-  def group_names(stat_files)
-    stat_files.map do |file|
-      Etc.getgrgid(file.gid).name
-    end
-  end
-
-  def last_update_date_times(stat_files)
-    stat_files.map do |file|
-      file.mtime.strftime('%b %d %H:%M')
-    end
-  end
-
-  def permissions(stat_files)
-    stat_files.map do |file|
-      mode = file.mode.to_s(8)
-      permission = mode[- 3..]
-      file_owner_permission = permission[0].to_i.to_s(2)
-      file_group_permission = permission[1].to_i.to_s(2)
-      other_permission = permission[2].to_i.to_s(2)
-      to_permission_string(file_owner_permission) + to_permission_string(file_group_permission) + to_permission_string(other_permission)
-    end
-  end
-
-  def to_permission_string(permission)
-    permission.split('').map.each_with_index do |string, index|
-      if string == '0'
-        '-'
-      elsif index.zero?
-        'r'
-      elsif index == 1
-        'w'
-      else
-        'x'
-      end
-    end.join('')
-  end
-
-  def file_types(files)
-    files.map do |file|
-      if file.ftype == 'file'
-        '-'
-      else
-        file.ftype[0]
-      end
     end
   end
 end
