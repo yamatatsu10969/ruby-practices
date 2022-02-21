c017e93594cbf8969a5005641cb489c4d536a80f
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-02-22 01:17:45 +0900

    git checkout で別ブランチに行ったらファイルの情報が書き変わったので修正

--- 05.ls/test/test_ls.rb
+++ 05.ls/test/test_ls.rb
@@ -60,13 +60,13 @@ class LsTest < Minitest::Test
   def test_ls_with_l_option
     expected = <<~TEXT
       total 32
-      -rwxr-xr-x  1 tatsuyayamamoto  staff     0 Feb 19 10:54 chmod_755_hoge.txt
-      -rwxrwxrwx  1 tatsuyayamamoto  staff     0 Feb 19 13:21 chmod_777_hoge.txt
-      -rw-r--r--  1 tatsuyayamamoto  staff     4 Feb 19 15:36 hoge_4byte.txt
-      -rw-r--r--  1 tatsuyayamamoto  staff  8193 Feb 19 15:42 hoge_8193byte.txt
-      drwxr-xr-x  3 tatsuyayamamoto  staff    96 Feb 19 11:50 hoge_directory
-      -rw-r--r--  1 root             staff     0 Feb 19 10:54 sudo_hoge.txt
-      lrwxr-xr-x  1 tatsuyayamamoto  staff    13 Feb 19 15:38 symlink_hoge -> sudo_hoge.txt
+      -rwxr-xr-x  1 tatsuyayamamoto  staff     0 Feb 22 01:07 chmod_755_hoge.txt
+      -rwxr-xr-x  1 tatsuyayamamoto  staff     0 Feb 22 01:07 chmod_777_hoge.txt
+      -rw-r--r--  1 tatsuyayamamoto  staff     4 Feb 22 01:07 hoge_4byte.txt
+      -rw-r--r--  1 tatsuyayamamoto  staff  8193 Feb 22 01:07 hoge_8193byte.txt
+      drwxr-xr-x  3 tatsuyayamamoto  staff    96 Feb 22 01:07 hoge_directory
+      -rw-r--r--  1 tatsuyayamamoto  staff     0 Feb 22 01:07 sudo_hoge.txt
+      lrwxr-xr-x  1 tatsuyayamamoto  staff    13 Feb 22 01:07 symlink_hoge -> sudo_hoge.txt
     TEXT
     Dir.chdir('../test_dir_option_l')
     assert_equal(expected.rstrip, LS.new(['l']).create_files_and_folders_text)
