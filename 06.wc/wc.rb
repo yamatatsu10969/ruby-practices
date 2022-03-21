f54db8310abbb9a634b3cb30293de520af8b48b9
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-21 23:07:05 +0900

    複数ファイルの入力に対応

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,19 +3,20 @@
 require 'etc'
 require 'optparse'
 
+DEFAULT_MAX_LENGTH = 8
+
 def run_wc
   paths = ARGV
-  first_path = paths[0]
-  # TODO: 三項演算子に変更
-  if first_path.nil?
+  if paths.empty?
     # TODO: file ではなく標準入力を処理
   else
-    data = p build_wc_data(first_path)
+    wc_items = paths.map { |path| build_wc_item_from(file_path: path) }
+    max_lengths = build_max_lengths(wc_items)
+    formatted_wc_items = wc_items.map { |wc_item| format_wc_item(wc_item, max_lengths) }
   end
-  puts format_wc_data(data)
 end
 
-def build_wc_data(file_path)
+def build_wc_item_from(file_path:)
   readlines = File.readlines(file_path)
   {
     lines: readlines.size,
@@ -25,14 +26,29 @@ def build_wc_data(file_path)
   }
 end
 
-def format_wc_data(wc_data)
-  # TODO: 8以上の桁数にも対応する
+def format_wc_item(wc_item, max_lengths)
   [
-    wc_data[:lines].to_s.rjust(8),
-    wc_data[:words].to_s.rjust(8),
-    wc_data[:bytes].to_s.rjust(8),
-    wc_data[:name]
+    wc_item[:lines].to_s.rjust(max_lengths[:lines]),
+    wc_item[:words].to_s.rjust(max_lengths[:words]),
+    wc_item[:bytes].to_s.rjust(max_lengths[:bytes]),
+    wc_item[:name]
   ].join(' ')
+end
+
+def build_max_lengths(wc_items)
+  {
+    lines: get_max_length(wc_items, :lines),
+    words: get_max_length(wc_items, :words),
+    bytes: get_max_length(wc_items, :bytes)
+  }
+end
+
+def get_max_length(wc_items, key)
+  [DEFAULT_MAX_LENGTH, find_max_length(wc_items, key)].max
+end
+
+def find_max_length(wc_items, key)
+  wc_items.map { |wc_item| wc_item[key].to_s.size }.max
 end
 
 run_wc
