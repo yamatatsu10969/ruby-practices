2c3b2f8c69995f7aab60019d8cb6f67ee83f327b
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-03-22 22:45:33 +0900

    複数ファイルの max_lengths を出すためには total のみ必要だったので wc_item のみを渡すように修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -19,23 +19,24 @@ end
 
 def format_from_standard_input(is_only_lines)
   wc_item = build_wc_item($stdin.readlines)
-  max_lengths = build_max_lengths([wc_item])
+  max_lengths = build_max_lengths(wc_item)
   format_wc_item(wc_item, max_lengths, is_only_lines)
 end
 
 def format_from_path(path, is_only_lines)
   wc_item = build_wc_item(File.readlines(path), path)
-  max_lengths = build_max_lengths([wc_item])
+  max_lengths = build_max_lengths(wc_item)
   format_wc_item(wc_item, max_lengths, is_only_lines)
 end
 
 def format_from_paths(paths, is_only_lines)
   wc_items = paths.map { |path| build_wc_item(File.readlines(path), path) }
-  max_lengths = build_max_lengths(wc_items)
-  formatted_wc_items = wc_items.map { |wc_item| format_wc_item(wc_item, max_lengths, is_only_lines) }
   total_wc_item = build_total_wc_item(wc_items)
-  formatted_total_item = format_wc_item(total_wc_item, build_max_lengths([total_wc_item]), is_only_lines)
-  [formatted_wc_items, formatted_total_item].join("\n")
+  # total の数が一番大きくなるため、total を元にmax_lengths を求める
+  max_lengths = build_max_lengths(total_wc_item)
+  wc_items << total_wc_item
+  formatted_wc_items = wc_items.map { |wc_item| format_wc_item(wc_item, max_lengths, is_only_lines) }
+  formatted_wc_items.join("\n")
 end
 
 def build_total_wc_item(wc_items)
@@ -66,18 +67,12 @@ def format_wc_item(wc_item, max_lengths, is_only_lines)
   ].join.rstrip
 end
 
-def build_max_lengths(wc_items)
-  max_lengths = {
-    lines: DEFAULT_MAX_LENGTH,
-    words: DEFAULT_MAX_LENGTH,
-    bytes: DEFAULT_MAX_LENGTH
+def build_max_lengths(wc_item)
+  {
+    lines: [DEFAULT_MAX_LENGTH, wc_item[:lines].to_s.size].max,
+    words: [DEFAULT_MAX_LENGTH, wc_item[:words].to_s.size].max,
+    bytes: [DEFAULT_MAX_LENGTH, wc_item[:bytes].to_s.size].max
   }
-  wc_items.each do |wc_item|
-    max_lengths[:lines] = [max_lengths[:lines], wc_item[:lines].to_s.size].max
-    max_lengths[:words] = [max_lengths[:words], wc_item[:words].to_s.size].max
-    max_lengths[:bytes] = [max_lengths[:bytes], wc_item[:bytes].to_s.size].max
-  end
-  max_lengths
 end
 
 puts run_wc
