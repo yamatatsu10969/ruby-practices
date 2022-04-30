7cd3931fb9aab0b125b810ade5e76ed789dfdeac
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-04-30 18:12:17 +0900

    frame に reader を付与

--- 07.bowling_object/lib/frame.rb
+++ 07.bowling_object/lib/frame.rb
@@ -3,6 +3,8 @@
 require_relative '../lib/shot'
 
 class Frame
+  attr_reader :first_shot, :second_shot
+
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark) if second_mark
