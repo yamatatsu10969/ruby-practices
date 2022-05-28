e8c48b0d67d6f6f100a6d963fcf3b47fa0e17d1d
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-28 10:57:59 +0900

    map と join を使うようにして余計な変数宣言を削除

--- 08.ls_object/lib/short_style.rb
+++ 08.ls_object/lib/short_style.rb
@@ -12,17 +12,14 @@ class ShortStyle
   end
 
   def format
-    formatted_texts = []
     column_item_number = @files.size / MAX_COLUMN_NUMBER
     max_text_length = @files.map(&:length).max + WHITE_SPACE_INDENT_LENGTH
-    (0..column_item_number).each do |column_index|
-      text = ''
+
+    (0..column_item_number).map do |column_index|
       (0..(MAX_COLUMN_NUMBER - 1)).map do |row_index|
         file_index = column_index + row_index + (row_index * column_item_number)
-        text += Kernel.format("%-#{max_text_length}s", @files[file_index])
-      end
-      formatted_texts.push(text)
-    end
-    formatted_texts.map(&:rstrip).join("\n")
+        Kernel.format("%-#{max_text_length}s", @files[file_index])
+      end.join
+    end.map(&:rstrip).join("\n")
   end
 end
