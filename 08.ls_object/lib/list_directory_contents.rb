f72321dbab2925154fdc394f7cc1c52bdfd7daa1
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-28 13:29:36 +0900

    LongStyle と ShortStyle はダックタイピングできたので修正

--- 08.ls_object/lib/list_directory_contents.rb
+++ 08.ls_object/lib/list_directory_contents.rb
@@ -29,7 +29,8 @@ class ListDirectoryContents
   def create_files_and_folders_text
     files = files_and_folders
     files = files.reverse if reverse_order?
-    show_long_format? ? LongStyle.new(files).format : ShortStyle.new(files).format
+    style = show_long_format? ? LongStyle : ShortStyle
+    style.new(files).format
   end
 
   def files_and_folders
